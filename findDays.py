0769c77901808448a34082d99ed1c143b4e42048
awsandgit <aws4em@gmail.com>
2023-04-19 10:32:23 +0530

    modified code

--- findDays.py
+++ findDays.py
@@ -13,13 +13,16 @@ def findDays(year,month):
         isLeap=0
 
     if (month == 2 and isLeap == 1 ):
-        print("29")
+        print("year:",year, "month:",month, "has 29 days")
     elif (month == 2 and isLeap == 0):
-        print("28")
+        #print("28")
+        print("year:",year, "month:",month, "has 28 days")
     elif (month in [1,3,5,7,8,10,12]):
-        print("31")
+        #print("31")
+        print("year:",year, "month:",month ,"has 31 days")
     elif (month in [4,6,9,11]):
-        print("30")
+       # print("30")
+        print("year:",year, "month:",month ,"has 30 days")
     else:
         print("enter valid month")
     return 1
